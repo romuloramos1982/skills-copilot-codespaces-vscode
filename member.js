6bf0f82143800fec123fc1da6ed38072eb78150d
romuloramos1982 <139821941+romuloramos1982@users.noreply.github.com>
2023-07-17 23:18:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl',
+    bindToController: true,
+    scope: {
+      member: '=',
+      skills: '='
+    }
+  };
+}
